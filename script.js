0b9ad665d79e97c84316f02149b8cb824078545e
Mishka0711 <michael82@yandex.ru>
2024-12-13 00:54:15 +0500

    добавил вывод в консоль

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const a = "Переменная из lesson03";
-const b = "Наша ветка lesson01";
-
-const logged = () => {
-  console.log("Наконец-то мы узнали все команды наобходиые для обучения");
-};
+console.log("Hello world");
